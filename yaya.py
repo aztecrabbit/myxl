572856c4a91edb5a442cf21983e4b0b495dac6d6
yogs89 <yoga9g30@gmail.com>
2022-10-16 20:36:36 +0700

    folder and file python

---
+++ yaya.py
@@ -0,0 +1,17 @@
+import threading
+
+def print_cube(num):
+	print("Cube: {}" .format(num * num * num))
+
+def print_square(num):
+    print("Square: {}" .format(num * num))
+
+if __name__ =="__main__":
+	t1 = threading.Thread(target=print_square, args=(10,))
+	t2 = threading.Thread(target=print_cube, args=(10,))
+	t1.start()
+	t2.start()
+	t1.join()
+	t2.join()
+
+	print("Done!")
